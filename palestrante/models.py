5d7ff707fa8e5302c2c9af4611c619137fe640ae
Gustavo de Oliveira Guerino <gustavoguerino@hotmail.com>
2018-06-26 23:28:11 -0300

    Adicionado meta aos models

--- palestrante/models.py
+++ palestrante/models.py
@@ -8,6 +8,8 @@ class Palestra(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.titulo
 
 class Pergunta(models.Model):
     palestra = models.ForeignKey(
@@ -16,3 +18,6 @@ class Pergunta(models.Model):
     autor = models.CharField(max_length=100)
     feita_em = models.DateTimeField(auto_now_add=True)
     aceita_em = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.autor, self.pergunta)
