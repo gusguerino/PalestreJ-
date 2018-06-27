e9830ae74a07f7003df9cced663494f0a34da8c6
Gustavo de Oliveira Guerino <gustavoguerino@hotmail.com>
2018-06-26 23:09:35 -0300

    Iniciando o projeto

---
+++ palestrante/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PalestranteConfig(AppConfig):
+    name = 'palestrante'
---
+++ palestrante/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+
+class Palestra(models.Model):
+    palestrante = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+
+
+class Pergunta(models.Model):
+    palestra = models.ForeignKey(
+        'Palestra', on_delete=models.CASCADE)
+    pergunta = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    feita_em = models.DateTimeField(auto_now_add=True)
+    aceita_em = models.DateTimeField(null=True)
